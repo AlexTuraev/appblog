a039dc2f70db82974a8c3e53e5acfec40d5b3fb2
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-03-01 12:49:02 +0300

    adding controller, servlet, WebConfig

---
+++ src/main/java/org/tasks/controller/BlogController.java
@@ -0,0 +1,21 @@
+package org.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/home")
+public class BlogController {
+
+    @GetMapping
+    @ResponseBody
+    public String getAll() {
+        return """
+                <h1>Hello</h1>
+                <h3>Blog</h3>
+                """;
+    }
+
+}
