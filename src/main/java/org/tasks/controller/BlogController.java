5e28b0cffea961517ffd89e12c63d279eba8eb35
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-03-11 19:34:57 +0300

    подготовка article (конкретный пост)

--- src/main/java/org/tasks/controller/BlogController.java
+++ src/main/java/org/tasks/controller/BlogController.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.tasks.dto.PostDto;
 import org.tasks.service.BlogService;
@@ -45,6 +47,13 @@ public class BlogController {
         }catch (Exception e) {
             return "redirect:/errorsave";
         }
+    }
+
+    @GetMapping("/{id}")
+    public String getPost(@PathVariable("id") long id, Model model) {
+//        PostDto post = blogService.getById();
+
+        return "article";
     }
 
 }
